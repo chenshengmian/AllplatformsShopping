cf7eb92353330c967d8e721251a49f3ebdb7b0b1
chenshengmian <2629701836@qq.com>
2023-10-14 18:02:29 +0800

    1014 main

--- components/know-yourCustomer/know-yourCustomer.vue
+++ components/know-yourCustomer/know-yourCustomer.vue
@@ -1,47 +1,24 @@
 <template>
 	<view>
 		<el-card>
-			<el-form :model="ruleForm"  :label-position="labelPosition"  :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
-				<el-form-item label="活动名称" prop="name">
-					<el-input v-model="ruleForm.name"></el-input>
+			<el-form :model="ruleForm"  :label-position="labelPosition"  :rules="rules" ref="ruleForm" label-width="200px" class="demo-ruleForm">
+				<el-form-item label="Identity card (Front) 身份证前" prop="idcard_pre">
+					<el-input v-model="ruleForm.idcard_pre"></el-input>
 				</el-form-item>
-				<el-form-item label="活动区域" prop="region">
-					<el-select v-model="ruleForm.region" placeholder="请选择活动区域">
-						<el-option label="区域一" value="shanghai"></el-option>
-						<el-option label="区域二" value="beijing"></el-option>
-					</el-select>
+				<el-form-item label="Account Holder 持卡者名字" prop="account_holder">
+					<el-input v-model="ruleForm.account_holder"></el-input>
 				</el-form-item>
-				<el-form-item label="活动时间" required>
-					<el-col :span="11">
-						<el-form-item prop="date1">
-							<el-date-picker type="date" placeholder="选择日期" v-model="ruleForm.date1"
-								style="width: 100%;"></el-date-picker>
-						</el-form-item>
-					</el-col>
-					<el-col class="line" :span="2">-</el-col>
-					<el-col :span="11">
-						<el-form-item prop="date2">
-							<el-time-picker placeholder="选择时间" v-model="ruleForm.date2"
-								style="width: 100%;"></el-time-picker>
-						</el-form-item>
-					</el-col>
+				<el-form-item label="Account Holder 持卡者名字" prop="account_holder">
+					<el-input v-model="ruleForm.account_holder"></el-input>
 				</el-form-item>
-				<el-form-item label="即时配送" prop="delivery">
-					<el-switch v-model="ruleForm.delivery"></el-switch>
+				<el-form-item label="Bank Name 银行名字" prop="bank_name">
+					<el-switch v-model="ruleForm.bank_name"></el-switch>
 				</el-form-item>
-				<el-form-item label="活动性质" prop="type">
-					<el-checkbox-group v-model="ruleForm.type">
-						<el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
-						<el-checkbox label="地推活动" name="type"></el-checkbox>
-						<el-checkbox label="线下主题活动" name="type"></el-checkbox>
-						<el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
-					</el-checkbox-group>
+				<el-form-item label="Bank Branch 银行分行" prop="bank_branch">
+					<el-switch v-model="ruleForm.bank_branch"></el-switch>
 				</el-form-item>
-				<el-form-item label="特殊资源" prop="resource">
-					<el-radio-group v-model="ruleForm.resource">
-						<el-radio label="线上品牌商赞助"></el-radio>
-						<el-radio label="线下场地免费"></el-radio>
-					</el-radio-group>
+				<el-form-item label="Bank Account Number 户口号码" prop="bank_account_number">
+					<el-switch v-model="ruleForm.bank_account_number"></el-switch>
 				</el-form-item>
 				<el-form-item label="活动形式" prop="desc">
 					<el-input type="textarea" v-model="ruleForm.desc"></el-input>
